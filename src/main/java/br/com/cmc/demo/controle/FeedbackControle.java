a4d52caf7714adac2b64a3f0c0411031c6d642b7
murilo <murilogabassan@gmail.com>
2025-01-22 10:34:02 -0400

    implementa função de listar e apagar feedbacks

--- src/main/java/br/com/cmc/demo/controle/FeedbackControle.java
+++ src/main/java/br/com/cmc/demo/controle/FeedbackControle.java
@@ -1,5 +1,7 @@
 package br.com.cmc.demo.controle;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -42,4 +45,20 @@ public class FeedbackControle {
         attributes.addFlashAttribute("mensagemFB", "Obrigado pelo feedback");
         return "redirect:/feedback/novo"; // Redireciona após sucesso
     }
+	
+	@GetMapping("/listar")
+	public String listarFB(Model model) {
+		List<Feedback> fBs = fbRepositorio.findAll();
+        
+		model.addAttribute("listaFB", fBs);
+		return "/auth/admin/lista-feedback";
+	}
+	
+	@GetMapping("/apagar/{id}")
+	public String apagarSB(@PathVariable("id") long id, Model model) {
+		Feedback fB = fbRepositorio.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Id inválido:" + id));
+		fbRepositorio.delete(fB);
+		return "redirect:/feedback/listar";
+	}
 }
