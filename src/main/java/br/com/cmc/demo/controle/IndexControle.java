5d6ef7b274a6c72411911551eb82472e1aaee7bf
murilo <murilogabassan@gmail.com>
2025-02-27 16:02:42 -0400

    Cadastro de usuário

--- src/main/java/br/com/cmc/demo/controle/IndexControle.java
+++ src/main/java/br/com/cmc/demo/controle/IndexControle.java
@@ -1,12 +1,30 @@
 package br.com.cmc.demo.controle;
 
+import java.util.Comparator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.cmc.demo.modelo.Feedback;
+import br.com.cmc.demo.modelo.SoftBox;
+import br.com.cmc.demo.modelo.Usuario;
+import br.com.cmc.demo.repositorio.UsuarioRepositorio;
 
 
 @Controller
 public class IndexControle {
+	
+	@Autowired
+	UsuarioRepositorio usRepositorio;
 
 	@GetMapping("/")
     public String pagInicial(){	
@@ -15,8 +33,32 @@ public class IndexControle {
     }
 	
 	@GetMapping("/login")
-    public String Login(){	
-	
+    public String Login(Model model){	
+		Usuario usuario = new Usuario();
+		usuario.setAtivo(true);
+        model.addAttribute("novoUser", usuario);	
 		return "/auth/login";
     }
+	
+	@PostMapping("/salvar")
+    public String salvar(@ModelAttribute("novoUser") @Validated Usuario User, BindingResult result,
+                           RedirectAttributes attributes, Model model) {
+        // Se houver erros de validação, redirecione de volta ao formulário
+        if (result.hasErrors()) {
+            return "/auth/login"; // Retorna ao formulário com erros
+        }
+
+        // Se passar nas validações, salva no repositório
+        usRepositorio.save(User);
+        attributes.addFlashAttribute("mensagemLogin", "Usuário cadastrado com Sucesso!");
+        return "redirect:/login"; // Redireciona após sucesso
+    }
+	
+	@GetMapping("/listar")
+	public String listarFB(Model model) {
+		List<Usuario> Uss = usRepositorio.findAll();
+        
+		model.addAttribute("listaUS", Uss);
+		return "/auth/admin/lista-usuario";
+	}
 }
--- src/main/java/br/com/cmc/demo/modelo/Usuario.java
+++ src/main/java/br/com/cmc/demo/modelo/Usuario.java
@@ -1,9 +1,15 @@
 package br.com.cmc.demo.modelo;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
@@ -13,11 +19,28 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank(message = "O nome é obrigatório")
+	private String nome;
+	
+	@NotBlank(message = "O CPF é obrigatório")
+	private String cpf;
+	
+	@NotBlank(message = "O e-mail é obrigatório")
+	private String email;
+	
 	@NotBlank(message = "O login é obrigatório")
 	private String login;
 	
 	@NotBlank(message = "A senha é obrigatória")
 	private String senha;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name="usuario_papel", 
+					 joinColumns = @JoinColumn(name = "usuario_id"),
+					 inverseJoinColumns = @JoinColumn(name = "papel_id"))
+	private List<Papel> papeis;
+	
+	private boolean ativo;
 
 	public String getLogin() {
 		return login;
@@ -42,5 +65,47 @@ public class Usuario {
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public List<Papel> getPapeis() {
+		return papeis;
+	}
+
+	public void setPapeis(List<Papel> papeis) {
+		this.papeis = papeis;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 
 }
